40e4a1f331c5a41dc1ab855d8b4ecce37c95184b
= <=dialloabdou@hotmail.com>
2023-11-23 13:57:44 +0100

    affichageCoordonnées

---
+++ Exercice4.js
@@ -0,0 +1,17 @@
+
+const v1 = {nom: "Paris", lat: 48.8534, long:2.34888};
+const v2 = {nom: "Toulouse", lat: 46.6043, long: 1.4437};
+const v3 = {nom: "Lyon", lat: 45.75, long: 4.45};
+
+AffichageCoordonnees(v1.nom, v1.lat,v1.long)
+AffichageCoordonnees(v2.nom, v2.lat,v2.long)
+AffichageCoordonnees(v3.nom, v3.lat,v3.long)
+
+function AffichageCoordonnees(nom, lat, long)
+{
+    console.log(`-----${nom}-----`)
+    console.log(`Latitude : ${lat}`)
+    console.log(`Longitude : ${long}`)
+
+
+}
